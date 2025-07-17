783f05a0346734a668de8917c9e01b5b2cd8acf3
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2025-07-17 15:48:32 +0100

    feat: merge server changes

--- gwas_catalog_data_mover.sh
+++ gwas_catalog_data_mover.sh
@@ -14,19 +14,17 @@
 #SBATCH -o "/nfs/production/opentargets/lsf/logs/ot_gwascat_gcp_rsync-%j.out"  # job output file
 #SBATCH -e "/nfs/production/opentargets/lsf/logs/ot_gwascat_gcp_rsync-%j.err"  # job error file
 
-
 # Bucket name for the outputs
 gcs_bucket="gwas_catalog_inputs"
 
 ############################################ SYNC SUMMARY STATISTICS AND METADATA TO GCP ############################################
 
-set -e  # Exit on error
-
+set -e # Exit on error
 
 # Sync paths
 target_path="gs://${gcs_bucket}/raw_summary_statistics/"
 base_path=/nfs/ftp/public/databases/gwas/summary_statistics/
-base_metadata_list_path="metadata_files_list.${date -I}.txt"
+base_metadata_list_path="metadata_files_list.$(date -I).txt"
 target_metadata_list_path="gs://${gcs_bucket}/sync_dump/${base_metadata_list_path}"
 
 # Software paths
@@ -41,11 +39,11 @@ path_ops_gcp_service_account="${path_ops_credentials}/gcp-service-account-gwas-s
 ${gsutil_path}/gcloud auth activate-service-account --key-file=${path_ops_gcp_service_account}
 
 # Sync all h.tsv.gz and h.tsv.gz.meta.yaml files
-${gsutil_path}/gsutil -m rsync -r -d -x '^(?!.*\.h\.tsv\.gz(.meta.yaml)?$)' ${base_path} ${target_path}
+${gsutil_path}/gsutil -u open-targets-genetics-dev -m rsync -r -d -x '^(?!.*\.h\.tsv\.gz(.meta.yaml)?$)' ${base_path} ${target_path}
 
 # Sync list of all metadata files
-find ${base_path} -type f -name "*.h.tsv.gz.meta.yaml" > $base_metadata_list_path
-${gsutil_path}/gsutil cp ${base_metadata_list_path} ${target_metadata_list_path}
+find ${base_path} -type f -name "*.h.tsv.gz.meta.yaml" >$base_metadata_list_path
+${gsutil_path}/gsutil -u open-targets-genetics-dev cp ${base_metadata_list_path} ${target_metadata_list_path}
 
 ############################################ READ METADATA AND COLLECT TO PARQUET FILE ############################################
 
@@ -63,4 +61,5 @@ ebi_to_gcp_path=./ebi-to-gcp
 ${ebi_to_gcp_path} $base_path $target_local_yaml_dump_path
 
 # Sync the yaml dump file
-${gsuitl_path}/gsutil cp ${target_local_yaml_dump_path} ${target_remove_yaml_dump_path}
+${gsuitl_path}/gsutil -u open-targets-genetics-dev cp ${target_local_yaml_dump_path} ${target_remove_yaml_dump_path}
+
